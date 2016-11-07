d00fc34ce3caeb8da914cb1f32c2528f7d77167c
Sudarshan <bsudarshan@outlook.com>
2016-11-07 18:47:39 -0500

    update

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,17 @@
+class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    def create
+        @article = Article.new(
+            title: params[:article][:title]
+            body: params[:article][:body])
+        @article.save
+        redirect_to article_path(@article)
+    end
+end
